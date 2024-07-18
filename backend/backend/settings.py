37d43c69fdc1c1556d66db1a8347e2f5666cf14b
Steven Sellers <steven.sellers@rocketmail.com>
2024-07-18 04:03:54 -0500

    Edit to allow all hosts for backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z2khhyikt88==hfyb$5tu(cff0qo33$67ue!!=-y-qxmk#nwgv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
